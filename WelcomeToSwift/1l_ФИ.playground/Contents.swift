083104bfc9864ed231d20c09e513a30713fdd8c6
nightsprings84 <s.o.d.metalist@gmail.com>
2020-12-14 17:45:21 +0300

    add-square-equetion1

--- WelcomeToSwift/1l_ФИ.playground/Contents.swift
+++ WelcomeToSwift/1l_ФИ.playground/Contents.swift
@@ -8,6 +8,17 @@ var discriminant = b * b - 4 * a * c
 var x = -b / 2 * a
 
 print(x)
+
+var a = 1
+var b = 12
+var c = 36
+var discriminant = b * b - 4 * a * c
+
+var x = -b / 2 * a
+
+print(x)
+
+
 // triangle
 
 var legOne = 3
@@ -24,3 +35,5 @@ print(hypotenuseOfTriangle)
 var perimeterOfTriangle = legOne + legTwo + (Int(hypotenuseOfTriangle))
 
 print(perimeterOfTriangle)
+
+
