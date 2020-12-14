223bf033dad740ae990f0afa27425c0e8e47b7d5
nightsprings84 <s.o.d.metalist@gmail.com>
2020-12-14 16:20:32 +0300

    Right-Triangle

--- WelcomeToSwift/1l_ФИ.playground/Contents.swift
+++ WelcomeToSwift/1l_ФИ.playground/Contents.swift
@@ -8,3 +8,19 @@ var discriminant = b * b - 4 * a * c
 var x = -b / 2 * a
 
 print(x)
+// triangle
+
+var legOne = 3
+var legTwo = 4
+
+var areaOfTriangle = legOne * legTwo / 2
+
+print(areaOfTriangle)
+
+var hypotenuseOfTriangle = sqrt(Double(((legOne * legOne)) + ((legTwo * legTwo))))
+
+print(hypotenuseOfTriangle)
+
+var perimeterOfTriangle = legOne + legTwo + (Int(hypotenuseOfTriangle))
+
+print(perimeterOfTriangle)
